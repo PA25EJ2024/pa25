027cd2514689a991a238e7400e5f2390970951a2
Daniel Salazar <sergio.salazarsnch@uanl.edu.mx>
2024-04-15 17:29:16 -0600

    undostres

--- codigo.py
+++ codigo.py
@@ -1,12 +1,10 @@
 print("Codigo gabriel")
 print("Codigo Diego")
 
-# tablas de multiplicar
-for i in range(1,11):
-    print(f"{8} X {i} = {8*i}")
-for i in range(1,11):
-    print(f"{25}X{i}={25*i}")
-for i in range (1,11):
-    print(f"5 x {i} = {5*i}")
-for x in range(1,11):
-    print(f"7 x {x} = {7*x}")
+
+# Daniel Salazar
+print("Tocar el bajo")
+print("Escuchar musica")
+print("Usar Linux")
+print("Ver peliculas")
+
