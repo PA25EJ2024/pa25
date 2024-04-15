b5187045a6b873874ae2e7b0a38ee64e8d0a6f63
MrRobis <RobertoEstradaSandoval@hotmail.com>
2024-04-15 17:25:26 -0600

    Yo

--- codigo.py
+++ codigo.py
@@ -10,3 +10,14 @@ for i in range (1,11):
     print(f"5 x {i} = {5*i}")
 for x in range(1,11):
     print(f"7 x {x} = {7*x}")
+
+
+
+
+
+
+#MrRobis
+print("Manejar en Simuladores")
+print("Jugar VIdeojuegos")
+print("ir al Gimnasio")
+print("Patinar en Skate")
