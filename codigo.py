2a1aa76845b2663660d757a5a174a0f681ebbcf3
Daniel Salazar <sergio.salazarsnch@uanl.edu.mx>
2024-04-15 17:13:14 -0600

    si jalo?

--- codigo.py
+++ codigo.py
@@ -2,17 +2,11 @@ print("Codigo gabriel")
 print("Codigo Diego")
 
 for i in range(1,11):
-    print(f"{8} X {i} = {25*i}")
-
-<<<<<<< HEAD
+    print(f"{8} X {i} = {8*i}")
 
 for x in range(1,11):
     print(f"7 x {x} = {7*x}")
-=======
+
 for i in range (1,11):
     print(f"5 x {i} = {5*i}")
-<<<<<<< HEAD
 
-======
->>>>>>> 611e209ff339e5e48d5455d2af391e710841edc4
->>>>>>> ac6b060ed355fb7d2e68a37ea60418496ca43e0d
